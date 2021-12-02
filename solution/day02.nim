7d9808b6d1049fb5a4f202ac36891ee41c193b7b
Alexandre Plateau <lexplt.dev@gmail.com>
2021-12-02 08:27:27 +0100

    adding day 2 part 1

---
+++ solution/day02.nim
@@ -0,0 +1,27 @@
+import utils
+
+import std/strutils, std/sequtils, std/strformat
+
+let lines = parseInput("02").map(proc(s: string): seq[string] = s.split(" "))
+
+proc part1(): int =
+    var
+        depth = 0
+        horizontal = 0
+
+    for inst in lines:
+        let
+            direction = inst[0]
+            count = parseInt(inst[1])
+
+        case direction:
+        of "forward":
+            horizontal += count
+        of "down":
+            depth += count
+        of "up":
+            depth -= count
+
+    return depth * horizontal
+
+echo fmt"Part1: {part1()}"
