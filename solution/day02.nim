770e057f9021048fae94fd0cb7180928cea12ed7
Alexandre Plateau <lexplt.dev@gmail.com>
2021-12-02 08:35:02 +0100

    solution to day 2 part 2!

--- solution/day02.nim
+++ solution/day02.nim
@@ -24,4 +24,27 @@ proc part1(): int =
 
     return depth * horizontal
 
+proc part2(): int =
+    var
+        aim = 0
+        depth = 0
+        horizontal = 0
+
+    for inst in lines:
+        let
+            direction = inst[0]
+            count = parseInt(inst[1])
+
+        case direction:
+        of "forward":
+            horizontal += count
+            depth += aim * count
+        of "down":
+            aim += count
+        of "up":
+            aim -= count
+
+    return depth * horizontal
+
 echo fmt"Part1: {part1()}"
+echo fmt"Part2: {part2()}"
