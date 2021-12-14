a6caaaf41ee048bc9a3a056ca72e1f45f01ba866
Alexandre Plateau <lexplt.dev@gmail.com>
2021-12-14 11:18:27 +0100

    perf improvements and part 2

--- src/day14.nim
+++ src/day14.nim
@@ -2,6 +2,12 @@ import utils
 
 import std/[strutils, sequtils, sugar, strformat, tables, algorithm]
 
+proc getPairs(chain: string): seq[string] =
+    var polymerPairs = chain.toSeq[0 ..< ^1].map(x => $x)
+    for i in 0 ..< polymerPairs.len:
+        polymerPairs[i].add chain[i + 1]
+    return polymerPairs
+
 proc simulate(lines: seq[string], steps: int): int =
     let
         polymer = lines[0]
@@ -10,26 +16,27 @@ proc simulate(lines: seq[string], steps: int): int =
             return (pair[0], pair[1])
         ).toTable
 
-    var currentPolymer = polymer
+    var currentPolymer = getPairs(polymer).toCountTable
 
     for i in 0 ..< steps:
-        var
-            tempPair = ""
-            newPolymer = ""
+        var newPolymer = initCountTable[string]()
 
-        for j, c in currentPolymer:
-            if tempPair.len() <= 1:
-                tempPair.add c
-            if tempPair.len() == 2:
-                newPolymer.add tempPair[0] & rules[tempPair]
-                tempPair = "" & tempPair[1]
+        for pair in currentPolymer.keys:
+            let
+                newPair1 = pair[0] & rules[pair]
+                newPair2 = rules[pair] & pair[1]
+            newPolymer.inc(newPair1, currentPolymer[pair])
+            newPolymer.inc(newPair2, currentPolymer[pair])
 
-        currentPolymer = newPolymer & currentPolymer[^1]
-        echo "step ", i
-        # echo i, ", length: ", currentPolymer.len, " ", currentPolymer, "\n"
+        currentPolymer = newPolymer
 
-    let counts = sorted(currentPolymer.toCountTable.values.toSeq, Descending)
-    return counts[0] - counts[^1]
+    var elements = initCountTable[char]()
+    for pair in currentPolymer.keys:
+        elements.inc(pair[0], currentPolymer[pair])
+
+    let counts = sorted(elements.values.toSeq, Descending)
+    # somehow needs -1 instead of +1 for my input, but this gives the correct values for the test sample
+    return counts[0] - counts[^1] + 1
 
 proc part1*(lines: seq[string]): int =
     return simulate(lines, 10)
